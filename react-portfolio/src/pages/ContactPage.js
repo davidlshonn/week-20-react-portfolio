62ea2152509fafa354f775f747936e740195d70b
David Shonn <davidlshonn@icloud.com>
2021-04-05 18:56:39 +0100

    api file added for contact form/not working

--- react-portfolio/src/pages/ContactPage.js
+++ react-portfolio/src/pages/ContactPage.js
@@ -3,6 +3,7 @@ import Content from "../components/Content";
 import Hero from "../components/Hero";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Axios from "axios";
 
 class ContactPage extends React.Component {
   constructor(props) {
@@ -17,7 +18,6 @@ class ContactPage extends React.Component {
   }
 
   handleChange = (event) => {
-    console.log(event);
     const target = event.target;
     const value = target.type === "checkbox" ? target.checked : target.value;
     const name = target.name;
@@ -29,15 +29,42 @@ class ContactPage extends React.Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
+
+    console.log(event.target);
+
     this.setState({
       disabled: true,
     });
+
+    Axios.post("http://localhost:3030/api/email", this.state)
+      .then((res) => {
+        if (res.data.success) {
+          this.setState({
+            disabled: false,
+            emailSent: true,
+          });
+        } else {
+          this.setState({
+            disabled: false,
+            emailSent: false,
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+
+        this.setState({
+          disabled: false,
+          emailSent: false,
+        });
+      });
   };
 
   render() {
     return (
       <div>
         <Hero title={this.props.title} />
+
         <Content>
           <Form onSubmit={this.handleSubmit}>
             <Form.Group>
@@ -50,35 +77,39 @@ class ContactPage extends React.Component {
                 onChange={this.handleChange}
               />
             </Form.Group>
+
             <Form.Group>
               <Form.Label htmlFor="email">Email</Form.Label>
               <Form.Control
                 id="email"
                 name="email"
                 type="email"
-                value={this.state.name}
+                value={this.state.email}
                 onChange={this.handleChange}
               />
             </Form.Group>
+
             <Form.Group>
               <Form.Label htmlFor="message">Message</Form.Label>
               <Form.Control
                 id="message"
                 name="message"
                 as="textarea"
-                rows="5"
-                value={this.state.name}
+                rows="3"
+                value={this.state.message}
                 onChange={this.handleChange}
               />
             </Form.Group>
+
             <Button
               className="d-inline-block"
               variant="primary"
               type="submit"
-              disabled="this.state.disabled"
+              disabled={this.state.disabled}
             >
               Send
             </Button>
+
             {this.state.emailSent === true && (
               <p className="d-inline success-msg">Email Sent</p>
             )}

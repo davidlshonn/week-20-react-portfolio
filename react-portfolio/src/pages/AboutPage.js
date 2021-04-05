4ff76c517faf5745af965b0a63a879c0129a4e1e
David Shonn <davidlshonn@icloud.com>
2021-04-05 01:49:53 +0100

    contact page layout implemented

--- react-portfolio/src/pages/AboutPage.js
+++ react-portfolio/src/pages/AboutPage.js
@@ -1,10 +1,25 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Content from "../components/Content";
 
 function AboutPage(props) {
   return (
     <div>
       <Hero title={props.title} />
+
+      <Content>
+        <p>
+          Currently studying part time at The University of Manchester Coding
+          Bootcamp, David also works as Web Designer at ITPS an IT Hardware
+          Reseller.
+        </p>{" "}
+        <p>
+          His focus throughout his education has been user-centered design,
+          studying Graphic Design at college and Transport Product Design at
+          Aston University. Although now would like a more technical role in
+          full stack development.
+        </p>
+      </Content>
     </div>
   );
 }
--- react-portfolio/src/pages/ContactPage.js
+++ react-portfolio/src/pages/ContactPage.js
@@ -1,7 +1,95 @@
 import React from "react";
+import Content from "../components/Content";
+import Hero from "../components/Hero";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
-function ContactPage(props) {
-  return <p>Contact Page</p>;
+class ContactPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      message: "",
+      disbled: false,
+      emailSent: null,
+    };
+  }
+
+  handleChange = (event) => {
+    console.log(event);
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      disabled: true,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Hero title={this.props.title} />
+        <Content>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Group>
+              <Form.Label htmlFor="full-name">Full Name</Form.Label>
+              <Form.Control
+                id="full-name"
+                name="name"
+                type="text"
+                value={this.state.name}
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label htmlFor="email">Email</Form.Label>
+              <Form.Control
+                id="email"
+                name="email"
+                type="email"
+                value={this.state.name}
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label htmlFor="message">Message</Form.Label>
+              <Form.Control
+                id="message"
+                name="message"
+                as="textarea"
+                rows="5"
+                value={this.state.name}
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+            <Button
+              className="d-inline-block"
+              variant="primary"
+              type="submit"
+              disabled="this.state.disabled"
+            >
+              Send
+            </Button>
+            {this.state.emailSent === true && (
+              <p className="d-inline success-msg">Email Sent</p>
+            )}
+            {this.state.emailSent === false && (
+              <p className="d-inline err-msg">Email Not Sent</p>
+            )}
+          </Form>
+        </Content>
+      </div>
+    );
+  }
 }
 
 export default ContactPage;
